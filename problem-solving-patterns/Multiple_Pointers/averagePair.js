c61ce51e917f72ad43f2d90aac631c6e306bcca1
bharathsampangi <bharathsampangi95@gmail.com>
2022-10-01 05:25:09 +0530

    average pair

---
+++ problem-solving-patterns/Multiple_Pointers/averagePair.js
@@ -0,0 +1,18 @@
+const averagePair = (array, targetSum) => {
+    if(array.length < 1) return false
+    let i=0, j = array.length - 1
+    while(i < j) {
+        if((array[i] + array[j]) == targetSum) {
+            return true
+        }
+        else if((array[i] + array[j]) > targetSum) {
+            j--
+        }
+        else {
+            i++
+        }
+    }
+    return false
+}
+
+console.log("average pair", averagePair([-1, 0, 3, 4, 5, 6], 4))

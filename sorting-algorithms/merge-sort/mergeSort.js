119dcb76de86a72b8a9f89366e077f8f3b35e8ef
bharath-spurtree <bharath.s@spurtreetech.in>
2022-12-06 10:13:11 +0530

    merge sort

--- sorting-algorithms/merge-sort/mergeSort.js
+++ sorting-algorithms/merge-sort/mergeSort.js
@@ -1,30 +1,35 @@
 function mergeSort(array) {
-
+    if(array.length <= 1) {
+        return array
+    }
+    const mid = Math.floor(array.length / 2)
+    const left = mergeSort(array.slice(0, mid))
+    const right = mergeSort(array.slice(mid))
+    return merge(left, right)
 }
 
 function merge(array1, array2) {
-    let newArray = []
+    const newArray = []
     let i=0, j=0
-    while(i<array1.length || j<array2.length) {
+    while(i<array1.length && j < array2.length) {
         if(array1[i] < array2[j]) {
             newArray.push(array1[i]);
             i++;
-            continue;
-        } else if(array2[j] < array1[i]) {
-            newArray.push(array2[j]);
-            j++;
-            continue;
-        }
-        if(array1[i]) {
-            newArray.push(array1[i]);
-            i++;
-        }
-        if(array2[j]) {
+        } else {
             newArray.push(array2[j]);
             j++;
         }
     }
+    while(i < array1.length) {
+        newArray.push(array1[i]);
+        i++;
+    }
+    while(j < array2.length) {
+        newArray.push(array2[j]);
+        j++;
+    }
     return newArray
 }
 
-console.log("merge", merge([1, 5, 50], [2, 14, 99, 100]))
+const mergeArrays = mergeSort([1, 14, 10, 99, 100, 5, 2])
+console.log("break array", mergeArrays)

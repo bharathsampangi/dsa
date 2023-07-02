e3f38b50c579e3c42f8795a3654a7c55ed82f811
bharath-spurtree <bharath.s@spurtreetech.in>
2023-07-02 06:31:33 +0530

    singly linked list

---
+++ datastructures/singlylinkedlist/SinglyLinkedList.js
@@ -0,0 +1,63 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class SinglyLinkedList{
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+    
+    push(val) {
+        const newNode = new Node(val)
+        this.length++
+        if(!this.head) {
+            this.head = newNode
+            this.tail = newNode
+            return
+        }
+        this.tail.next = newNode
+        this.tail = newNode
+    }
+
+    pop() {
+        if(!this.head || !this.tail) 
+            return
+        if(this.head === this.tail) {
+            this.head = null
+            this.tail = null
+            this.length = 0
+            return
+        }
+        let current = this.head
+        let prev = null
+        for(let i=0; i < this.length; i++) {
+            if(current === this.tail) {
+                prev.next = null
+            } else {
+                prev = current
+                current = current.next
+            }
+        }
+        this.length--
+        return
+    }
+
+    shift() {
+        
+    }
+}
+
+const singleList = new SinglyLinkedList()
+singleList.push("Hello")
+singleList.push("How")
+
+console.log(singleList)
+
+singleList.pop()
+
+console.log(singleList)

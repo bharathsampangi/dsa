da83d94c3f1a07852f4ee1a2d3c510add4e4a87b
bharathsampangi <bharathsampangi95@gmail.com>
2025-01-14 18:33:48 +0530

    anagram

--- practice/anagram.js
+++ practice/anagram.js
@@ -4,6 +4,31 @@
 // Note: You may assume the string contains only lowercase alphabets.
 
 // Time Complexity - O(n)
+
+function validAnagram(str1, str2) {
+    let map = {}
+
+    for(let c of str1) {
+        if(!map[c]) {
+            map[c] = 1
+            continue
+        }
+        map[c] = map[c] + 1
+    }
+
+    for(let c of str2) {
+        if(!map[c])
+            break
+        if(map[c] == 1) {
+            delete map[c]
+            continue
+        }
+        map[c] = map[c] - 1
+    }
+    
+    console.log(map)
+    return Object.keys(map).length === 0
+}
  
 console.log(validAnagram('anagram', 'nagaram')); // true
 console.log(validAnagram('rat', 'car')); // false

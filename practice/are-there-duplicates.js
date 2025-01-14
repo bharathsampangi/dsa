311403dfb018a4c39cc7b489d7b38b00da11479e
bharathsampangi <bharathsampangi95@gmail.com>
2025-01-14 19:21:54 +0530

    duplicates and average pair

--- practice/are-there-duplicates.js
+++ practice/are-there-duplicates.js
@@ -1,7 +1,25 @@
+// frequency counter
 // Implement a function called areThereDuplicates which accepts a variable number of arguments,
 // and checks whether there are any duplicates among the arguments passed in.
 
 // Restrictions: Time Complexity - O(n), Space Complexity - O(n)
+
+function areThereDuplicates(...array) {
+    const map = {}
+
+    for(let i=0; i < array.length; i++) {
+        let item = array[i]
+        if(map[item] == 1) {
+            return true
+        }
+        if(!map[item]) {
+            map[item] = 1
+            continue
+        }
+    }
+
+    return false
+}
   
 console.log(areThereDuplicates(1, 2, 3)); // false
 console.log(areThereDuplicates('a', 'b', 'c', 'a')); // true
--- practice/average-pair.js
+++ practice/average-pair.js
@@ -6,6 +6,25 @@
 // Bonus Constraints:
 // Time Complexity: O(N)
 // Space Complexity: O(1)
+
+function averagePair(array, target) {
+    if(!array.length)
+        return false
+    if(array.length == 1)
+        return target == array[0]
+
+    let auxSum = array[0] + array[1]
+    if(auxSum === target)
+        return true
+
+    for(let i=2; i < array.length; i++) {
+        auxSum = auxSum + array[i] - array[i - 2]
+        if(target == auxSum)
+            return true
+    }
+
+    return false
+}
   
 console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
 console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false

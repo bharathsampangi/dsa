6cd108f7d5d1036e60e26e2eb51932e888ab9e4e
bharathsampangi <bharathsampangi95@gmail.com>
2025-01-24 02:09:05 +0530

    remove

--- practice/capitalize-first.js
+++ practice/capitalize-first.js
@@ -1,5 +1,6 @@
 // capitalizeFirst
-// Write a recursive function called capitalizeFirst.
 // Given an array of strings, capitalize the first letter of each string in the array.
+
+
 
 console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']

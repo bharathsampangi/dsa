7af339e42080fbfcb01fa8bc043ef25c39fa6c44
bharathsampangi <bharathsampangi95@gmail.com>
2025-02-04 05:29:22 +0530

    factorial

--- practice/factorial.js
+++ practice/factorial.js
@@ -4,6 +4,13 @@
 // e.g., factorial four ( 4! ) is equal to 24, because 4 * 3 * 2 * 1 equals 24.
 // factorial zero (0!) is always 1.
 
+function factorial(n) {
+    if(n == 0 || n == 1)
+        return 1
+
+    return n * factorial(n - 1)
+}
+
 console.log(factorial(1)); // 1
 console.log(factorial(2)); // 2
 console.log(factorial(4)); // 24

90f09ffca4d1436197a3f57d90719ac1cc60ff05
bharathsampangi <bharathsampangi95@gmail.com>
2025-02-04 05:11:21 +0530

    done

--- practice/capitalize-first.js
+++ practice/capitalize-first.js
@@ -1,6 +1,16 @@
 // capitalizeFirst
+// Write a function called capitalizeFirst.
 // Given an array of strings, capitalize the first letter of each string in the array.
 
+function capitalizeFirst(array) {
+    let result = []
 
+    if(!array.length)
+        return result
+
+    result.push(array[0][0].toUpperCase() + array[0].slice(1))
+
+    return result.concat(capitalizeFirst(array.slice(1)))
+}
 
 console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
--- practice/capitalize-words.js
+++ practice/capitalize-words.js
@@ -1,6 +1,17 @@
 // capitalizeWords
-// Write a recursive function called capitalizeWords.
+// Write a function called capitalizeWords.
 // Given an array of words, return a new array containing each word capitalized.
+
+function capitalizeWords(array) {
+    let result = []
+
+    if(!array.length)
+        return result
+
+    result.push(array[0].toUpperCase())
+
+    return result.concat(capitalizeWords(array.slice(1)))
+}
 
 const words = ["i", "am", "learning", "recursion"];
 console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']

fb5f250aef45c352463d55c4cc246bd350e8943b
bharathsampangi <bharathsampangi95@gmail.com>
2025-02-10 09:33:22 +0530

    find duplicates

--- practice/find-all-duplicates.js
+++ practice/find-all-duplicates.js
@@ -3,6 +3,21 @@
 // Note that you can return the elements in any order.
 
 // Time Complexity - O(n)
+
+function findAllDuplicates(arr) {
+    const duplicates = []
+    let obj = {}
+
+    for(let item of arr) {
+        if(item in obj) {
+            duplicates.push(item)
+        } else {
+            obj[item] = 1
+        }
+    }
+
+    return duplicates
+}
   
 console.log(findAllDuplicates([4, 3, 2, 1, 0])); // []
 console.log(findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3])); // array with 3, 2 and 1

92edf0cb072e7e76c0c9794c2b8bcc6ffc0e1a6e
bharathsampangi <bharathsampangi95@gmail.com>
2025-02-13 15:03:15 +0530

    find longest substring

--- practice/find-longest-substring.js
+++ practice/find-longest-substring.js
@@ -3,11 +3,28 @@
 
 // Time Complexity - O(n^2)
 
-console.log(findLongestSubstringVersion1("")); // 0
-console.log(findLongestSubstringVersion1("rithmschool")); // 7
-console.log(findLongestSubstringVersion1("thisisawesome")); // 6
+// console.log(findLongestSubstringVersion1("")); // 0
+// console.log(findLongestSubstringVersion1("rithmschool")); // 7
+// console.log(findLongestSubstringVersion1("thisisawesome")); // 6
 
 // Time Complexity - O(n)
+
+function findLongestSubstringVersion2(str) {
+    let max = 0
+    let start = 0
+    let obj = {}
+
+    for(let i=0; i < str.length; i++) {
+        if(obj[str[i]]) {
+            start = obj[str[i]] = Math.max(start, obj[str[i]])
+        }
+
+        obj[str[i]] = i + 1
+        max = Math.max(max, i - start + 1)
+    }
+
+    return max
+}
 
 console.log(findLongestSubstringVersion2("")); // 0
 console.log(findLongestSubstringVersion2("rithmschool")); // 7
--- practice/find-pair.js
+++ practice/find-pair.js
@@ -1,8 +1,45 @@
-// Given an array of positive integers, some elements appear twice and others appear once.
-// Find all the elements that appear twice in this array.
-// Note that you can return the elements in any order.
+// Given an unsorted array and a number n, find if there exists a pair of elements
+// in the array whose difference is n. This function should return true
+// if the pair exists or false if it does not.
 
-// Time Complexity - O(n)
-  
-console.log(findAllDuplicates([4, 3, 2, 1, 0])); // []
-console.log(findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3])); // array with 3, 2 and 1
+// Solve this with the following requirements:
+// Time Complexity - O(n log n)
+// Space Complexity - O(1)
+
+const findPair = (arr, numsDiff) => {
+    arr.sort((a,b) => a - b)
+    let i=0
+    let j = 1
+    numsDiff = Math.abs(numsDiff)
+    let currDiff = 0
+
+    while(j < arr.length) {
+        currDiff = Math.abs(arr[i] - arr[j])
+        if(currDiff === numsDiff)
+            return true
+
+        if(currDiff > numsDiff && i === j -1) {
+            i++;
+            j++;
+        }
+        else if(currDiff > numsDiff) {
+            i++
+        } else {
+            j++
+        }
+    }
+
+    return false
+};
+
+console.log(findPair([6, 1, 4, 10, 2, 4], 2)); // true
+console.log(findPair([8, 6, 2, 4, 1, 0, 2, 5, 13], 1)); // true
+console.log(findPair([4, -2, 3, 10], -6)); // true
+console.log(findPair([6, 1, 4, 10, 2, 4], 22)); // false
+console.log(findPair([], 0)); // false
+console.log(findPair([5, 5], 0)); // true
+console.log(findPair([-4, 4], -8)); // true
+console.log(findPair([-4, 4], 8)); // true
+console.log(findPair([1, 3, 4, 6], -2)); // true
+console.log(findPair([0, 1, 3, 4, 6], -2)); // true
+console.log(findPair([-2, 5, 10, 15, 16], 0)); // false

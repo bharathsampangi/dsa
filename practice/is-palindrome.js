9c66ea23eb36326c8da4d7963ed817abf01828e8
bharathsampangi <bharathsampangi95@gmail.com>
2025-02-13 23:50:40 +0530

    palindrome

--- practice/is-palindrome.js
+++ practice/is-palindrome.js
@@ -3,5 +3,18 @@
 // if the string passed to it is a palindrome (reads the same forward and backward).
 // Otherwise it returns false.
 
+function isPalindrome(str) {
+    let i=0, j = str.length - 1
+
+    while(i<j) {
+        if(str[i] !== str[j])
+            return false
+        i++
+        j--
+    }
+
+    return true
+}
+
 console.log(isPalindrome("amanaplanacanalpanama")); // true
 console.log(isPalindrome("amanaplanacanalpandemonium")); // false

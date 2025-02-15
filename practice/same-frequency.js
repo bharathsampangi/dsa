af03159111fed2f5f6606856078889f1c6e66703
bharathsampangi <bharathsampangi95@gmail.com>
2025-02-15 08:09:37 +0530

    some recursive

--- practice/same-frequency.js
+++ practice/same-frequency.js
@@ -2,6 +2,13 @@
 // find out if the two numbers have the same frequency of digits.
 
 // Your solution MUST have the following complexities: Time: O(N)
+
+function sameFrequency(num1, num2) {
+    const map = {}
+    while(num1 >= 0) {
+        
+    }
+}
   
 console.log(sameFrequency(34, 14)); // false
 console.log(sameFrequency(3589578, 5879385)); // true
--- practice/some-recursive.js
+++ practice/some-recursive.js
@@ -3,6 +3,12 @@
 // The function returns true if a single value in the array returns true
 // when passed to the callback. Otherwise it returns false.
 
+function someRecursive(arr, func) {
+    if(arr.length == 1)
+        return func(arr[0])
+    return func(arr[0]) || someRecursive(arr.slice(1), func)
+}
+
 console.log(someRecursive([1, 2, 3, 4], (val) => val % 2 !== 0)); // true
 console.log(someRecursive([4, 6, 8, 9], (val) => val % 2 !== 0)); // true
 console.log(someRecursive([4, 6, 8], (val) => val % 2 !== 0)); // false

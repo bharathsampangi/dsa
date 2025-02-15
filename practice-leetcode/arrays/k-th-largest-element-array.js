5a1f50b324f10c6786b4cc9637a8bb0be3a2ff2d
bharathsampangi <bharathsampangi95@gmail.com>
2025-02-16 00:48:38 +0530

    2 problems

--- practice-leetcode/arrays/k-th-largest-element-array.js
+++ practice-leetcode/arrays/k-th-largest-element-array.js
@@ -4,8 +4,6 @@
 
 // Can you solve it without sorting?
 
- 
-
 // Example 1:
 
 // Input: nums = [3,2,1,5,6,4], k = 2
@@ -20,3 +18,95 @@
 
 // 1 <= k <= nums.length <= 105
 // -104 <= nums[i] <= 104
+
+class MinHeap {
+    constructor() {
+        this.values = []
+    }
+
+    enqueue(ele) {
+        this.values.push(ele)
+        this.bubbleUp(ele)
+    }
+
+    bubbleUp(element) {
+        let i = this.values.length - 1
+        while(i > 0) {
+            let currIdx = Math.floor((i - 1) / 2)
+            let current = this.values[currIdx]
+            
+            if(current > element) {
+                this.values[i] = current
+                this.values[currIdx] = element
+                i = currIdx
+            } else {
+                break;
+            }
+        }
+    }
+
+    dequeue() {
+        let max = this.values[0]
+        let element = this.values.pop()
+        if(this.values.length > 0) {
+            this.values[0] = element
+            this.sinkDown()
+        }
+        return max
+    }
+
+    sinkDown() {
+        let index = 0
+        let element = this.values[index]
+        let length = this.values.length
+        
+        while(true) {
+            let leftIndex = (2 * index) + 1
+            let rightIndex = (2 * index) + 2
+            let leftChild, rightChild
+            let swap = null
+
+            if(leftIndex < length) {
+                leftChild = this.values[leftIndex]
+                if(leftChild < element) {
+                    swap = leftIndex
+                }
+            }
+            if(rightIndex < length) {
+                rightChild = this.values[rightIndex]
+                if((swap === null && rightChild < element) || (swap !== null && rightChild < leftChild)) {
+                    swap = rightIndex
+                }
+            }
+            if(swap === null) break
+            this.values[index] = this.values[swap]
+            this.values[swap] = element
+            index = swap
+        }
+    }
+
+    peek() {
+        return this.values[0]
+    }
+}
+
+function kthLargestElementArray(nums, k) {
+    if(!nums.length || k < 0) return
+    const heap = new MinHeap()
+
+    for(let i=0; i < k; i++) {
+        heap.enqueue(nums[i])
+    }
+
+    for(let j=k; j < nums.length; j++) {
+        if(nums[j] > heap.peek()) {
+            heap.dequeue()
+            heap.enqueue(nums[j])
+        }
+    }
+
+    return heap.peek()
+}
+
+console.log(kthLargestElementArray([3,2,1,5,6,4], 2))
+console.log(kthLargestElementArray([3,2,3,1,2,4,5,5,6], 4))
--- practice-leetcode/arrays/maximum-subarray.js
+++ practice-leetcode/arrays/maximum-subarray.js
@@ -1,3 +1,4 @@
+// Medium
 // Given an integer array nums, find the 
 // subarray
 //  with the largest sum, and return its sum.
@@ -25,4 +26,26 @@
 // -104 <= nums[i] <= 104
  
 
-// Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+// Follow up: If you have figured out the O(n) solution, 
+// try coding another solution using the divide and conquer approach, which is more subtle.
+
+var maxSubArray = function(nums) {
+    let max = nums[0]
+    let sum = 0
+
+    for(let i=0; i < nums.length; i++) {
+        sum = sum + nums[i]
+        if(sum > max) {
+            max = sum
+        }
+        if(sum < 0) {
+            sum = 0
+        }
+    }
+
+    return max
+};
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+console.log(maxSubArray([1]))
+console.log(maxSubArray([5,4,-1,7,8]))

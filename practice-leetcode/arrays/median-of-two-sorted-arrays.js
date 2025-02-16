284c15f3386226e0d80695d51b23031c7e3aff21
bharathsampangi <bharathsampangi95@gmail.com>
2025-02-17 03:43:17 +0530

    median of two sorted

--- practice-leetcode/arrays/median-of-two-sorted-arrays.js
+++ practice-leetcode/arrays/median-of-two-sorted-arrays.js
@@ -1,30 +1,69 @@
-// Hard 
-// Given an array of positive integers nums and a positive integer target, return the minimal length of a 
-// subarray
-//  whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+// Hard
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+// The overall run time complexity should be O(log (m+n)).
 
  
 
 // Example 1:
 
-// Input: target = 7, nums = [2,3,1,2,4,3]
-// Output: 2
-// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+// Input: nums1 = [1,3], nums2 = [2]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
 // Example 2:
 
-// Input: target = 4, nums = [1,4,4]
-// Output: 1
-// Example 3:
-
-// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
-// Output: 0
+// Input: nums1 = [1,2], nums2 = [3,4]
+// Output: 2.50000
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
  
 
 // Constraints:
 
-// 1 <= target <= 109
-// 1 <= nums.length <= 105
-// 1 <= nums[i] <= 104
- 
+// nums1.length == m
+// nums2.length == n
+// 0 <= m <= 1000
+// 0 <= n <= 1000
+// 1 <= m + n <= 2000
+// -106 <= nums1[i], nums2[i] <= 106
 
-// Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+var findMedianSortedArrays = function(nums1, nums2) {
+    let idx1 = 0
+    let idx2 = 0
+    const merged = []
+
+    while(idx1 < nums1.length && idx2 < nums2.length) {
+        if(nums1[idx1] === nums2[idx2]) {
+            merged.push(nums1[[idx1]], nums2[idx2])
+            idx1++
+            idx2++
+        }
+        else if(nums1[idx1] < nums2[idx2]) {
+            merged.push(nums1[idx1])
+            idx1++
+        } else {
+            merged.push(nums2[idx2])
+            idx2++
+        }
+    }
+
+    while(idx1 < nums1.length) {
+        merged.push(nums1[idx1])
+        idx1++
+    }
+
+    while(idx2 < nums2.length) {
+        merged.push(nums2[idx2])
+        idx2++
+    }
+
+    if(merged.length % 2 === 0) {
+        let mid = Math.floor(merged.length / 2)
+        return (merged[mid] + merged[mid - 1]) / 2
+    } else {
+        let mid = Math.floor((merged.length) / 2)
+        return merged[mid]
+    }
+};
+
+console.log(findMedianSortedArrays([1, 2], [3]))
+console.log(findMedianSortedArrays([1, 2], [3, 4]))

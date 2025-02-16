c2db676f2d8d7a080b3951e9690584226478160c
bharathsampangi <bharathsampangi95@gmail.com>
2025-02-16 11:11:55 +0530

    maximum product subarray

--- practice-leetcode/arrays/maximum-product-subarray.js
+++ practice-leetcode/arrays/maximum-product-subarray.js
@@ -23,3 +23,36 @@
 // 1 <= nums.length <= 2 * 104
 // -10 <= nums[i] <= 10
 // The product of any subarray of nums is guaranteed to fit in a 32-bit integer.
+
+var maxProduct = function(nums) {
+    let max2 = -Infinity
+    let max = -Infinity
+    let prod = 1
+
+    for(let i=0; i < nums.length; i++) {
+        let num = nums[i]
+        prod = prod * num
+        if(prod > max) {
+            max = prod
+        }
+        if(prod === 0) {
+            prod = 1
+        } 
+    }
+    prod = 1
+    for(let j=nums.length - 1; j > 0; j--) {
+        let num = nums[j]
+        prod = prod * num
+        if(prod > max2) {
+            max2 = prod
+        }
+        if(prod == 0) {
+            prod = 1
+        }
+    }
+
+    return Math.max(max, max2)
+};
+
+console.log(maxProduct([2,3,-2,4]))
+console.log(maxProduct([-2,0,-1]))

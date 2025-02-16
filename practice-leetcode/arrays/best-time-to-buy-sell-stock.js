606660fe9a19627958069506c68f86e79fc65283
bharathsampangi <bharathsampangi95@gmail.com>
2025-02-16 18:45:07 +0530

    best time to buy sell stock

--- practice-leetcode/arrays/best-time-to-buy-sell-stock.js
+++ practice-leetcode/arrays/best-time-to-buy-sell-stock.js
@@ -1,6 +1,7 @@
 // You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day 
+// in the future to sell that stock.
 
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
@@ -21,3 +22,19 @@
 
 // 1 <= prices.length <= 105
 // 0 <= prices[i] <= 104
+
+var maxProfit = function(prices) {
+    let minPrice = Infinity
+    let maxProfit = -Infinity
+
+    for(let i=0; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i])
+        let profit = prices[i] - minPrice
+        maxProfit = Math.max(profit, maxProfit)
+    }
+
+    return maxProfit
+};
+
+console.log(maxProfit([7,1,5,3,6,4]))
+console.log(maxProfit([7,6,4,3,1]))
--- practice-leetcode/arrays/container-with-most-water.js
+++ practice-leetcode/arrays/container-with-most-water.js
@@ -24,3 +24,7 @@
 // n == height.length
 // 2 <= n <= 105
 // 0 <= height[i] <= 104
+
+var maxArea = function(height) {
+    
+};

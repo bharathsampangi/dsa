31860b90b20575895a4f10f9a326d5c2ff06f45d
bharathsampangi <bharathsampangi95@gmail.com>
2025-04-09 10:46:52 +0530

    refactor

--- datastructures/dynamic-programming/max-sum-non-adjacent.js
+++ datastructures/dynamic-programming/max-sum-non-adjacent.js
@@ -1,8 +1,10 @@
 // Problem statement
-// You are given an array/list of ‘N’ integers. You are supposed to return the maximum sum of the subsequence with the constraint that no two elements are adjacent in the given array/list.
+// You are given an array/list of ‘N’ integers. You are supposed to return the maximum sum 
+// of the subsequence with the constraint that no two elements are adjacent in the given array/list.
 
 // Note:
-// A subsequence of an array/list is obtained by deleting some number of elements (can be zero) from the array/list, leaving the remaining elements in their original order.
+// A subsequence of an array/list is obtained by deleting some number of elements (can be zero) 
+// from the array/list, leaving the remaining elements in their original order.
 
 function calcSum(idx, nums, dp) {
     if(idx === 0) return nums[idx]
